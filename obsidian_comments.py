6c7a75e34e8f5bc2e5d0d5fa105884e98f52f1c0
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2024-06-04 13:21:18 -0300

    obsidian_comments: ignore text inside "%%"

---
+++ obsidian_comments.py
@@ -0,0 +1,31 @@
+import regex
+
+
+COMMENT_PATTERN = regex.compile(r"%%[^%]%%")
+
+def on_page_markdown(markdown: str, *_):
+	remove_text: list[tuple[int, int]] = []
+
+	start = 0
+	inside_comment = False
+
+	while True:
+		found_at = markdown.find("%%", start)
+
+		if found_at == -1:
+			if inside_comment:
+				remove_text.append((start - 1, len(markdown) - 1))
+			break
+
+		inside_comment = not inside_comment
+
+		if not inside_comment:
+			remove_text.append((start - 1, found_at + 1))
+
+		start = found_at + 1
+
+	return "".join(
+		l
+		for i, l in enumerate(markdown)
+		if not any( s <= i <= end for s, end in remove_text)
+	)
